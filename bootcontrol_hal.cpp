80f071f87c6ebcde1cde46125718acd75a8d54bd
Dmytro Prokopchuk <dmytro.prokopchuk@globallogic.com>
2018-06-27 17:37:05 +0300

    Implemented VNDK support for BootControl HAL
    Change-Id: Ic51eaacaacea6274f895ecd1782d9f741b7e0a7f
    Signed-off-by: Dmytro Prokopchuk <dmytro.prokopchuk@globallogic.com>
    Jira-ID: ADM-1531

--- bootcontrol_hal.cpp
+++ bootcontrol_hal.cpp
@@ -19,6 +19,18 @@
 #include <errno.h>
 #include <string.h>
 #include <exception>
+#include <endian.h>
+#include <fcntl.h>
+
+#include <string>
+
+#include <android-base/file.h>
+#include <android-base/logging.h>
+#include <android-base/properties.h>
+#include <android-base/stringprintf.h>
+#include <android-base/unique_fd.h>
+#include <hardware/boot_control.h>
+#include <hardware/hardware.h>
 
 #include <utils/Log.h>
 #include <cutils/properties.h>
@@ -32,56 +44,206 @@ namespace V1_0 {
 namespace renesas {
 
 BootControl::BootControl()
-    : m_ops(nullptr), m_is_init(false) {
+    : m_current_slot_index(GetCurrentSlotIndex()) {
     ALOGD("Create BootControl");
-    m_ops = avb_ops_user_new();
-    if (m_ops == nullptr) {
-        ALOGE("Unable to allocate AvbOps instance.");
-    } else {
-        m_is_init = true; //init done
+
+    if (m_current_slot_index == AVB_AB_ERROR_SLOT_INDEX) {
+        ALOGE("Unable to initialize BootControl");
     }
 };
 
 BootControl::~BootControl() {
     ALOGD("Destroy BootControl");
-    if (m_ops) {
-        avb_ops_user_free(m_ops);
+};
+
+uint32_t BootControl::GetCurrentSlotIndex(void) {
+    /* Initialize the current_slot from the read-only property */
+    std::string suffix_prop = android::base::GetProperty(
+            AVB_AB_PROP_SLOT_SUFFIX, "");
+
+    return SlotSuffixToIndex(suffix_prop.c_str());
+};
+
+/* Return the index of the slot suffix passed
+ * or AVB_AB_ERROR_SLOT_INDEX if not a valid slot suffix
+ */
+uint32_t BootControl::SlotSuffixToIndex(const char* suffix) {
+    for (uint32_t slot = 0; slot < AVB_AB_MAX_SLOTS; ++slot) {
+        if (!strncmp(AVB_AB_SLOT_SUFFIXIES[slot], suffix, 2)) {
+            return slot;
+        }
     }
+    return AVB_AB_ERROR_SLOT_INDEX;
 };
 
-//Returns the number of available slots
-Return<uint32_t> BootControl::getNumberSlots() {
-    return NUM_SLOTS;
+bool BootControl::SlotIsBootable(AvbABSlotData* slot_data) {
+    return (slot_data->priority > 0)
+            && (slot_data->successful_boot
+                    || (slot_data->tries_remaining > 0));
 };
 
-//Returns the slot number of that the current boot is booted from
-Return<uint32_t> BootControl::getCurrentSlot() {
-    char propbuf[PROPERTY_VALUE_MAX] = {0};
-
-    property_get("ro.boot.slot_suffix", propbuf, "");
-    if (strcmp(propbuf, "_a") == 0) {
-        return 0;
-    } else if (strcmp(propbuf, "_b") == 0) {
-        return 1;
+/* Ensure all unbootable and/or illegal states are marked as the
+ * canonical 'unbootable' state, e.g. priority=0, tries_remaining=0,
+ * and successful_boot=0
+ */
+void BootControl::SlotNormalize(AvbABSlotData* slot_data) {
+    if (slot_data->priority > 0) {
+        if (slot_data->tries_remaining == 0 && !slot_data->successful_boot) {
+            /* We've exhausted all tries -> unbootable */
+            SlotSetUnbootable(slot_data);
+        }
+        if (slot_data->tries_remaining > 0 && slot_data->successful_boot) {
+            /* Illegal state - avb_ab_mark_slot_successful() will clear
+             * tries_remaining when setting successful_boot
+             */
+            SlotSetUnbootable(slot_data);
+        }
     } else {
-        ALOGE("Unexpected slot suffix.");
+        SlotSetUnbootable(slot_data);
     }
-    return 0;
 };
 
-//Marks the current slot as having booted successfully
+void BootControl::SlotSetUnbootable(AvbABSlotData* slot_data) {
+    slot_data->priority = 0;
+    slot_data->tries_remaining = 0;
+    slot_data->successful_boot = 0;
+};
+
+bool BootControl::ValidateAvbABData(const AvbABData* ab_data) {
+    /* Ensure magic is correct */
+    if (strncmp(reinterpret_cast<const char*>(ab_data->magic), AVB_AB_MAGIC,
+            AVB_AB_MAGIC_LEN)) {
+        ALOGE("Magic number is incorrect");
+        return false;
+    }
+
+    /* Ensure we don't attempt to access any fields if the major version
+     * is not supported
+     */
+    if (ab_data->version_major > AVB_AB_MAJOR_VERSION) {
+        ALOGE("No support for given major version");
+        return false;
+    }
+
+    /* Bail if CRC32 doesn't match */
+    if (ab_data->crc32 != CalculateAvbABDataCRC(ab_data)) {
+        ALOGE("CRC32 does not match");
+        return false;
+    }
+
+    return true;
+};
+
+uint32_t BootControl::CRC32(const uint8_t* buf, size_t size) {
+    static uint32_t crc_table[256];
+
+    /* Compute the CRC-32 table only once */
+    if (!crc_table[1]) {
+        for (uint32_t i = 0; i < 256; ++i) {
+            uint32_t crc = i;
+            for (uint32_t j = 0; j < 8; ++j) {
+                uint32_t mask = -(crc & 1);
+                crc = (crc >> 1) ^ (0xEDB88320 & mask);
+            }
+            crc_table[i] = crc;
+        }
+    }
+
+    uint32_t ret = -1;
+    for (size_t i = 0; i < size; ++i) {
+        ret = (ret >> 8) ^ crc_table[(ret ^ buf[i]) & 0xFF];
+    }
+
+    return ~ret;
+};
+
+/* Return the little-endian representation of the CRC-32 */
+uint32_t BootControl::CalculateAvbABDataCRC(const AvbABData* ab_data) {
+    return htole32(CRC32(reinterpret_cast<const uint8_t*>(ab_data),
+            offsetof(AvbABData, crc32)));
+};
+
+bool BootControl::LoadAvbABData(const char* misc_device, AvbABData* ab_data) {
+    android::base::unique_fd fd(open(misc_device, O_RDONLY));
+    if (fd.get() == -1) {
+        ALOGE("Failed to open '/mics' partition");
+        return false;
+    }
+    if (lseek(fd, AVB_AB_METADATA_MISC_PARTITION_OFFSET, SEEK_SET)
+            != AVB_AB_METADATA_MISC_PARTITION_OFFSET) {
+        ALOGE("Failed to seek '/mics' partition");
+        return false;
+    }
+    if (!android::base::ReadFully(fd.get(), ab_data, sizeof(AvbABData))) {
+        ALOGE("Failed to read '/mics' partition");
+        return false;
+    }
+
+    if (!ValidateAvbABData(ab_data)) {
+        return false;
+    }
+#if 0
+    for (uint32_t slot = 0; slot < AVB_AB_MAX_SLOTS; ++slot) {
+        SlotNormalize(&ab_data->slots[slot]);
+    }
+#endif
+    return true;
+};
+
+bool BootControl::UpdateAndSaveAvbABData(const char* misc_device,
+        AvbABData* ab_data) {
+    ab_data->crc32 = CalculateAvbABDataCRC(ab_data);
+
+    android::base::unique_fd fd(open(misc_device, O_WRONLY | O_SYNC));
+    if (fd.get() == -1) {
+        ALOGE("Failed to open '/mics' partition");
+        return false;
+    }
+    if (lseek(fd.get(), AVB_AB_METADATA_MISC_PARTITION_OFFSET, SEEK_SET)
+            != AVB_AB_METADATA_MISC_PARTITION_OFFSET) {
+        ALOGE("Failed to seek '/mics' partition");
+        return false;
+    }
+    if (!android::base::WriteFully(fd.get(), ab_data, sizeof(AvbABData))) {
+        ALOGE("Failed to write '/mics' partition");
+        return false;
+    }
+
+    return true;
+};
+
+/* Returns the number of available slots */
+Return<uint32_t> BootControl::getNumberSlots() {
+    return AVB_AB_MAX_SLOTS;
+};
+
+/* Returns the slot number of that the current boot is booted from */
+Return<uint32_t> BootControl::getCurrentSlot() {
+    return m_current_slot_index;
+};
+
+/* Marks the current slot as having booted successfully */
 Return<void> BootControl::markBootSuccessful(markBootSuccessful_cb _hidl_cb) {
     int ret = -EIO;
 
-    if (m_is_init) {
-        uint32_t currSlot = getCurrentSlot();
-        if (avb_ab_mark_slot_successful(m_ops->ab_ops, currSlot) ==
-            AVB_IO_RESULT_OK) {
-            ret = 0;
+    if (m_current_slot_index != AVB_AB_ERROR_SLOT_INDEX) {
+
+        AvbABData ab_data;
+        if (LoadAvbABData(AVB_AB_PROP_MISC_DEVICE, &ab_data)) {
+
+            if (SlotIsBootable(&ab_data.slots[m_current_slot_index])) {
+
+                ab_data.slots[m_current_slot_index].successful_boot = 1;
+                ab_data.slots[m_current_slot_index].tries_remaining = 0;
+
+                if (UpdateAndSaveAvbABData(AVB_AB_PROP_MISC_DEVICE, &ab_data)) {
+                    ret = 0;
+                }
+
+            } else {
+                ALOGE("Cannot mark unbootable slot as successful");
+            }
         }
-    } else {
-        ALOGE("AvbOps instance was not allocated.");
-        ret = -ENOMEM;
     }
 
     struct CommandResult cr;
@@ -91,21 +253,34 @@ Return<void> BootControl::markBootSuccessful(markBootSuccessful_cb _hidl_cb) {
     return Void();
 };
 
-//Marks the slot passed in parameter as the active boot slot
+/* Marks the slot passed in parameter as the active boot slot */
 Return<void> BootControl::setActiveBootSlot(uint32_t slot,
         setActiveBootSlot_cb _hidl_cb) {
-    int ret = -EIO;
+    int ret = -EINVAL;
 
-    if (m_is_init) {
-        if (slot < getNumberSlots()) {
-            if (avb_ab_mark_slot_active(m_ops->ab_ops, slot) ==
-                AVB_IO_RESULT_OK) {
+    if (slot < getNumberSlots()) {
+        ret = -EIO;
+        AvbABData ab_data;
+        if (LoadAvbABData(AVB_AB_PROP_MISC_DEVICE, &ab_data)) {
+
+            /* Make requested slot top priority, unsuccessful,
+             * and with max tries
+             */
+            ab_data.slots[slot].priority = AVB_AB_MAX_PRIORITY;
+            ab_data.slots[slot].tries_remaining =
+                    AVB_AB_MAX_TRIES_REMAINING;
+            ab_data.slots[slot].successful_boot = 0;
+
+            /* Ensure other slot doesn't have as high a priority. */
+            uint32_t other_slot = 1 - slot;
+            if (ab_data.slots[other_slot].priority == AVB_AB_MAX_PRIORITY) {
+                ab_data.slots[other_slot].priority = AVB_AB_MAX_PRIORITY - 1;
+            }
+
+            if (UpdateAndSaveAvbABData(AVB_AB_PROP_MISC_DEVICE, &ab_data)) {
                 ret = 0;
             }
         }
-    } else {
-        ALOGE("AvbOps instance was not allocated.");
-        ret = -ENOMEM;
     }
 
     struct CommandResult cr;
@@ -115,21 +290,22 @@ Return<void> BootControl::setActiveBootSlot(uint32_t slot,
     return Void();
 };
 
-//Marks the slot passed in parameter as an unbootable
+/* Marks the slot passed in parameter as an unbootable */
 Return<void> BootControl::setSlotAsUnbootable(uint32_t slot,
         setSlotAsUnbootable_cb _hidl_cb) {
-    int ret = -EIO;
+    int ret = -EINVAL;
 
-    if (m_is_init) {
-        if (slot < getNumberSlots()) {
-            if (avb_ab_mark_slot_unbootable(m_ops->ab_ops, slot) ==
-                AVB_IO_RESULT_OK) {
+    if (slot < getNumberSlots()) {
+        ret = -EIO;
+        AvbABData ab_data;
+        if (LoadAvbABData(AVB_AB_PROP_MISC_DEVICE, &ab_data)) {
+
+            SlotSetUnbootable(&ab_data.slots[slot]);
+
+            if (UpdateAndSaveAvbABData(AVB_AB_PROP_MISC_DEVICE, &ab_data)) {
                 ret = 0;
             }
         }
-    } else {
-        ALOGE("AvbOps instance was not allocated.");
-        ret = -ENOMEM;
     }
 
     struct CommandResult cr;
@@ -139,66 +315,56 @@ Return<void> BootControl::setSlotAsUnbootable(uint32_t slot,
     return Void();
 };
 
-//Returns if the slot passed in parameter is bootable
+/* Returns if the slot passed in parameter is bootable */
 Return<BoolResult> BootControl::isSlotBootable(uint32_t slot) {
-    if (!m_is_init) {
-        ALOGE("AvbOps instance was not allocated.");
-        return BoolResult::INVALID_SLOT;
-    }
+
     if (slot >= getNumberSlots()) {
-        //The slot does not exist
         return BoolResult::INVALID_SLOT;
     }
 
     AvbABData ab_data;
-    bool is_bootable = false;
 
-    if (avb_ab_data_read(m_ops->ab_ops, &ab_data) != AVB_IO_RESULT_OK) {
+    if (!LoadAvbABData(AVB_AB_PROP_MISC_DEVICE, &ab_data)) {
         return BoolResult::INVALID_SLOT;
     }
 
-    is_bootable = (ab_data.slots[slot].priority > 0) &&
-        (ab_data.slots[slot].successful_boot ||
-        (ab_data.slots[slot].tries_remaining > 0));
-
-    //Returns TRUE if the slot is bootable, FALSE if it's not
-    return is_bootable ? BoolResult::TRUE : BoolResult::FALSE;
+    /* Returns TRUE if the slot is bootable, FALSE if it's not */
+    return SlotIsBootable(&ab_data.slots[slot]) ?
+        BoolResult::TRUE : BoolResult::FALSE;
 };
 
-//Returns if the slot passed in parameter has been marked as successful
-//using markBootSuccessful()
+/* Returns if the slot passed in parameter has been marked as successful
+ * using markBootSuccessful()
+ */
 Return<BoolResult> BootControl::isSlotMarkedSuccessful(uint32_t slot) {
-    if (!m_is_init) {
-        ALOGE("AvbOps instance was not allocated.");
-        return BoolResult::INVALID_SLOT;
-    }
+
     if (slot >= getNumberSlots()) {
-        //The slot does not exist
         return BoolResult::INVALID_SLOT;
     }
 
     AvbABData ab_data;
     bool is_marked_successful = false;
 
-    if (avb_ab_data_read(m_ops->ab_ops, &ab_data) != AVB_IO_RESULT_OK) {
+    if (!LoadAvbABData(AVB_AB_PROP_MISC_DEVICE, &ab_data)) {
         return BoolResult::INVALID_SLOT;
     }
 
     is_marked_successful = ab_data.slots[slot].successful_boot;
 
-    //Returns TRUE if slot has been marked as successful, FALSE if it has not
+    /* Returns TRUE if slot has been marked as successful, FALSE if it has not */
     return is_marked_successful ? BoolResult::TRUE : BoolResult::FALSE;
 };
 
-//Returns the string suffix used by partitions
+/* Returns the string suffix used by partitions */
 Return<void> BootControl::getSuffix(uint32_t slot, getSuffix_cb _hidl_cb) {
-    static const char* suffix[2] = {"_a", "_b"};
-    //Returns the empty string "" if slot does not match an existing slot.
+
+    /* Returns the empty string "" if slot does not match an existing slot */
     hidl_string ans("");
 
     if (slot < getNumberSlots()) {
-        ans = suffix[slot];
+        ans = AVB_AB_SLOT_SUFFIXIES[slot];
     }
+
     _hidl_cb(ans);
     return Void();
 };
--- bootcontrol_hal.h
+++ bootcontrol_hal.h
@@ -17,13 +17,14 @@
 #ifndef BOOTCONTROL_HAL_H_
 #define BOOTCONTROL_HAL_H_
 
+#include <assert.h>
+#include <stddef.h>
+#include <stdint.h>
+
 #include <hidl/Status.h>
 #include <hidl/MQDescriptor.h>
 
 #include <android/hardware/boot/1.0/IBootControl.h>
-
-#include <libavb_user/libavb_user.h>
-#include <libavb_ab/avb_ab_flow.h>
 
 namespace android {
 namespace hardware {
@@ -44,22 +45,116 @@ public:
     BootControl();
     ~BootControl();
 
-    // Methods from ::android::hardware::boot::V1_0::IBootControl follow.
+    /* Methods from ::android::hardware::boot::V1_0::IBootControl follow */
     Return<uint32_t> getNumberSlots() override;
     Return<uint32_t> getCurrentSlot() override;
     Return<void> markBootSuccessful(markBootSuccessful_cb _hidl_cb) override;
-    Return<void> setActiveBootSlot(uint32_t slot,
-        setActiveBootSlot_cb _hidl_cb) override;
+    Return<void> setActiveBootSlot(uint32_t slot, setActiveBootSlot_cb _hidl_cb)
+            override;
     Return<void> setSlotAsUnbootable(uint32_t slot,
-        setSlotAsUnbootable_cb _hidl_cb) override;
+            setSlotAsUnbootable_cb _hidl_cb) override;
     Return<BoolResult> isSlotBootable(uint32_t slot) override;
     Return<BoolResult> isSlotMarkedSuccessful(uint32_t slot) override;
     Return<void> getSuffix(uint32_t slot, getSuffix_cb _hidl_cb) override;
 
 private:
-    const uint32_t NUM_SLOTS = 2; ///< number of available slots (A/B)
-    AvbOps* m_ops; ///< high-level operations structure
-    bool m_is_init; ///< initialization flag
+
+    static const uint32_t AVB_AB_MAGIC_LEN = 4;
+
+    /* Number of available slots (A/B) present on the device */
+    static const uint32_t AVB_AB_MAX_SLOTS = 2;
+
+    /* Magic for the A/B struct when serialized */
+    const char* AVB_AB_MAGIC = "\0AB0";
+
+    /* Versioning for the on-disk A/B metadata - keep in sync with avbtool */
+    const uint32_t AVB_AB_MAJOR_VERSION = 1;
+    const uint32_t AVB_AB_MINOR_VERSION = 0;
+
+    /* Maximum values for slot data */
+    const uint32_t AVB_AB_MAX_PRIORITY = 15;
+    const uint32_t AVB_AB_MAX_TRIES_REMAINING = 7;
+
+    /* AvbABData struct is stored in 2048 bytes offset
+     * into the 'misc' partition
+     */
+    const off_t AVB_AB_METADATA_MISC_PARTITION_OFFSET = 2048;
+
+    const char* AVB_AB_SLOT_SUFFIXIES[AVB_AB_MAX_SLOTS] = { "_a", "_b" };
+
+    const char* AVB_AB_PROP_SLOT_SUFFIX = "ro.boot.slot_suffix";
+
+    /* The path to the misc device */
+    const char* AVB_AB_PROP_MISC_DEVICE =
+            "/dev/block/platform/soc/ee140000.sd/by-name/misc";
+
+    const uint32_t AVB_AB_ERROR_SLOT_INDEX = 0xABCDFFFF;
+
+    /* Struct used for recording per-slot A/B metadata.
+     *
+     * When serialized, data is stored in network byte-order.
+     */
+    typedef struct AvbABSlotData {
+        /* Slot priority. Valid values range from herr0 to AVB_AB_MAX_PRIORITY,
+         * both inclusive with 1 being the lowest and AVB_AB_MAX_PRIORITY
+         * being the highest. The special value 0 is used to indicate the
+         * slot is unbootable.
+         */
+        uint8_t priority;
+
+        /* Number of times left attempting to boot this slot ranging from 0
+         * to AVB_AB_MAX_TRIES_REMAINING.
+         */
+        uint8_t tries_remaining;
+
+        /* Non-zero if this slot has booted successfully, 0 otherwise. */
+        uint8_t successful_boot;
+
+        /* Reserved for future use. */
+        uint8_t reserved[1];
+    }__attribute__((packed)) AvbABSlotData;
+
+    /* Struct used for recording A/B metadata.
+     *
+     * When serialized, data is stored in network byte-order.
+     */
+    typedef struct AvbABData {
+        /* Magic number used for identification - see AVB_AB_MAGIC. */
+        uint8_t magic[AVB_AB_MAGIC_LEN];
+
+        /* Version of on-disk struct - see AVB_AB_{MAJOR,MINOR}_VERSION. */
+        uint8_t version_major;
+        uint8_t version_minor;
+
+        /* Padding to ensure |slots| field start eight bytes in. */
+        uint8_t reserved1[2];
+
+        /* Per-slot metadata. */
+        AvbABSlotData slots[2];
+
+        /* Reserved for future use. */
+        uint8_t reserved2[12];
+
+        /* CRC32 of all 28 bytes preceding this field in LE format. */
+        uint32_t crc32;
+    }__attribute__((packed)) AvbABData;
+
+    static_assert(sizeof(struct AvbABData) == 32,
+            "struct AvbABData size changed, must be equal 32 bytes");
+
+    uint32_t GetCurrentSlotIndex(void);
+    uint32_t SlotSuffixToIndex(const char* suffix);
+    bool SlotIsBootable(AvbABSlotData* slot_data);
+    void SlotNormalize(AvbABSlotData* slot_data);
+    void SlotSetUnbootable(AvbABSlotData* slot_data);
+    uint32_t CalculateAvbABDataCRC(const AvbABData* ab_data);
+    uint32_t CRC32(const uint8_t* buf, size_t size);
+    bool LoadAvbABData(const char* misc_device, AvbABData* ab_data);
+    bool UpdateAndSaveAvbABData(const char* misc_device, AvbABData* ab_data);
+    bool ValidateAvbABData(const AvbABData* ab_data);
+
+    /* The slot where we are running from */
+    uint32_t m_current_slot_index;
 };
 
 } // namespace renesas
